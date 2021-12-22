98d22be6762ae3d77c2dc945a19307f5bd695bb7
nikkon2001@mail.ru <nikkon2301>
2021-12-22 21:17:52 +0500

    Обработка нажатий кнопок цифр

--- app/src/main/java/com/example/calculator_project/MainActivity.java
+++ app/src/main/java/com/example/calculator_project/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.calculator_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        findViewById(R.id.button_1).setOnClickListener(this);
+        findViewById(R.id.button_2).setOnClickListener(this);
+        findViewById(R.id.button_3).setOnClickListener(this);
+        findViewById(R.id.button_4).setOnClickListener(this);
+        findViewById(R.id.button_5).setOnClickListener(this);
+        findViewById(R.id.button_6).setOnClickListener(this);
+        findViewById(R.id.button_7).setOnClickListener(this);
+        findViewById(R.id.button_8).setOnClickListener(this);
+        findViewById(R.id.button_9).setOnClickListener(this);
+        findViewById(R.id.button_0).setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        TextView textView = findViewById(R.id.text_view);
+        Button button = (Button) v;
+        if(textView.getText().length() == 11){
+            Toast.makeText(MainActivity.this,"Слишком длинное число",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String s = (String) textView.getText() + button.getText();
+        textView.setText(s);
     }
 }
